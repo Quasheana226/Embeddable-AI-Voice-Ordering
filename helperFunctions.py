6670e7f2df8c3d4a4e3cf61df03094b9c058ffa1
Quasheana226 <quasheana@gmail.com>
2026-06-09 18:06:33 -0400

    Step 1: Implement speech_to_text function with IBM Watson STT

--- helperFunctions.py
+++ helperFunctions.py
@@ -18,6 +18,7 @@ def clean_text(text):
     # TODO
     return None
 
+
 def get_keywords(text):
     # TODO
     return None
@@ -35,7 +36,6 @@ def read_zip_file(zip_name):
     files = str(os.getcwd() + "/" + zip_name + ".zip")
     with ZipFile(files, "r") as zip_object:
         zip_object.extractall()
-
     path = str(os.getcwd() + "/" + zip_name + "/*.wav")
     folder = glob.glob(path)
     return sorted(folder, reverse=False)
@@ -44,12 +44,21 @@ def read_zip_file(zip_name):
 def save_audio(file, file_name):
     with open(file_name, "wb") as audio:
         file.save(audio)
-        print("file uploaded successfully")
+    print("file uploaded successfully")
 
 
 def speech_to_text(file_name):
-    # TODO
-    return None
+    speech_to_text_url = "https://sn-watson-stt.labs.skills.network/speech-to-text/api/v1/recognize"
+    headers = {"Content-Type": "audio/wav"}
+    params = {"model": "en-US_Multimedia", "smart_formatting": "true", "background_audio_suppression": "0.6"}
+    result = requests.post(speech_to_text_url, headers=headers, params=params, data=open(file_name, 'rb'))
+    output = ""
+    json_obj = json.loads(result.text)
+    results_data = json_obj["results"]
+    for r in results_data:
+        for transcript in r["alternatives"]:
+            output = output + " " + transcript["transcript"]
+    return output
 
 
 def text_to_speech(texts, name, language):
